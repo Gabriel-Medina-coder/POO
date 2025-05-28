0a217fa57765fc062e555d5f07f856af91530459
Gabriel-Medina-coder <20243ds040@utez.edu.mx>
2025-05-28 07:47:55 -0600

    Creacion de metodos,getters y set para modelo From:DEV

--- U1_TI_04_Computadora/src/Computadora.java
+++ U1_TI_04_Computadora/src/Computadora.java
@@ -1,6 +1,29 @@
 public class Computadora {
     private String brand;
     private String model;
-    private boolean status;
-    
+    private boolean status = true;
+
+    public void turnOn(){
+
+    }
+
+    public void turnOff(){
+
+    }
+//GETTERS
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+//SETTER
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
