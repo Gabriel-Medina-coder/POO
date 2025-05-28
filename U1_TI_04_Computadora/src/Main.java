b68d6d2046b5149a30c0a69523210fbee287e89d
Gabriel-Medina-coder <20243ds040@utez.edu.mx>
2025-05-28 08:03:28 -0600

    Correcciones

--- U1_TI_04_Computadora/src/Main.java
+++ U1_TI_04_Computadora/src/Main.java
@@ -1,12 +1,10 @@
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
         Computadora computer = new Computadora();
 
         computer.setModel("Inspiron 15");
 
-        System.out.println("Marca: "computer.getBrand());
+        System.out.println("Marca: " + computer.getBrand());
         System.out.println("Modelo:" + computer.getModel());
 
         computer.turnOn();
