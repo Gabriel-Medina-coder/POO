f96beb232bc57c41b0999dceb0a57e2b3fceb807
AntonioAcevedo1178 <20243ds076@utez.edu.mx>
2025-05-30 08:37:22 -0600

    Definicion de getters y setters para cada atributo

--- U1_TI_05_Persona/src/Persona.java
+++ U1_TI_05_Persona/src/Persona.java
@@ -3,5 +3,28 @@ public class Persona {
     private double height;
     private double weight;
 
-    
+    //GETTERS
+    public String getName() {
+        return name;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    //SETTERS
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    //Calcular
 }
