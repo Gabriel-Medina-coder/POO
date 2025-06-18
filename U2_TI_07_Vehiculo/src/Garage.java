b50c78a90acfc4be944011efc4f6af71d3562528
Gabriel-Medina-Coder <20243ds040@utez.edu.mx>
2025-06-18 08:16:29 -0600

    Creación del programa de vehiculo

---
+++ U2_TI_07_Vehiculo/src/Garage.java
@@ -0,0 +1,40 @@
+import javax.print.DocFlavor;
+
+public class Garage{
+    private int numEspacios;
+    private Vehiculo[] espacios;
+
+    public Garage(int numEspacios) {
+        espacios=new Vehiculo[numEspacios];
+    }
+
+
+
+    public boolean estacionar(Vehiculo v){
+        for (int i = 0; i < numEspacios; i++) {
+            if(espacios[i] == null){
+                espacios[i]=v;
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean retirar(String placa){
+        for (int i = 0; i < numEspacios; i++) {
+            if (espacios[i].getPlaca().equals(placa)) {
+                espacios[i]=null;
+                return true;
+            }
+        }
+        return false;
+    }
+    public int contarVehiculos() {
+        int cantVehiculo=0;
+        for (int i = 0; i < numEspacios; i++) {
+            if(espacios[i]!=null){
+                cantVehiculo++;
+            }
+        }
+        return cantVehiculo;
+    }
+}
