9fa06dab11b2873e80bc01010705037599ed74c8
Gabriel-Medina-coder <20243ds040@utez.edu.mx>
2025-06-18 23:15:59 -0600

    Constructor y definicion de atributos de clase animal

--- U2_TI_08_Tienda_Mascotas/src/Animal.java
+++ U2_TI_08_Tienda_Mascotas/src/Animal.java
@@ -1,2 +1,9 @@
 public abstract class Animal{
+    private String nombre;
+    private int edad;
+
+    public Animal(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
 }
