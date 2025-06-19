7d9e8a226fca1b02ae6ab254ef5c5bc90e123575
Gabriel-Medina-coder <20243ds040@utez.edu.mx>
2025-06-19 00:38:58 -0600

    Clase producto, atributos y setters con validación

--- U2_TI_09_Gestor_Inventario/src/Producto.java
+++ U2_TI_09_Gestor_Inventario/src/Producto.java
@@ -1,2 +1,29 @@
 public class Producto {
+    private String codigo;
+    private String nombre;
+    private int cantidad;
+
+    public void setCodigo(String codigo) {
+       if (codigo.isEmpty()) {
+           System.out.println("El codigo no puede estar vacio");
+       }else  {
+           this.codigo = codigo;
+       }
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre.isEmpty()) {
+            System.out.println("El nombre no puede estar vacio");
+        }else  {
+            this.nombre = nombre;
+        }
+    }
+
+    public void setCantidad(int cantidad) {
+        if (cantidad >= 0) {
+            this.cantidad = cantidad;
+        }else   {
+            System.out.println("Debe existir minimo 1 producto");
+        }
+    }
 }
