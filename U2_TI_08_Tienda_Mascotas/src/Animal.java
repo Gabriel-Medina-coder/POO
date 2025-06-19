b6ee17c461e7ff782fe1d67590ac2433379b8fb3
Gabriel-Medina-coder <20243ds040@utez.edu.mx>
2025-06-18 23:34:40 -0600

    Clase Cliente, metodos y lista

--- U2_TI_08_Tienda_Mascotas/src/Animal.java
+++ U2_TI_08_Tienda_Mascotas/src/Animal.java
@@ -6,4 +6,22 @@ public abstract class Animal{
         this.nombre = nombre;
         this.edad = edad;
     }
+
+    //Getters and Setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
---
+++ U2_TI_08_Tienda_Mascotas/src/Cliente.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Cliente {
+    private String nombre;
+
+    List<Animal> mascotas;
+}
